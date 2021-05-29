30e5cd9b879c1e95e7c3a238bd23e1b7872c58f3
Vijay Rudraraju <vijay326@gmail.com>
2021-05-29 10:32:37 -0700

    Fix some audio bugs

--- client/src/audio.js
+++ client/src/audio.js
@@ -8,105 +8,105 @@ const DEFAULT_BPM = 56;
 const DEFAULT_SEQUENCE = ['*', '*', '*', '*'];
 
 const casioKeys = new Tone.Players({
-  urls: {
-    0: 'A1.mp3',
-    1: 'Cs2.mp3',
-    2: 'E2.mp3',
-    3: 'Fs2.mp3',
-  },
-  fadeOut: '64n',
-  baseUrl: 'https://tonejs.github.io/audio/casio/',
+    urls: {
+        0: 'A1.mp3',
+        1: 'Cs2.mp3',
+        2: 'E2.mp3',
+        3: 'Fs2.mp3',
+    },
+    fadeOut: '64n',
+    baseUrl: 'https://tonejs.github.io/audio/casio/',
 }).toDestination();
 
 const salamanderKeys = new Tone.Sampler({
-  urls: {
-    C1: 'C1.mp3',
-    C2: 'C2.mp3',
-    C3: 'C3.mp3',
-    C4: 'C4.mp3',
-    'D#4': 'Ds4.mp3',
-    'F#4': 'Fs4.mp3',
-    A4: 'A4.mp3',
-  },
-  volume: -8,
-  release: 1,
-  baseUrl: 'https://tonejs.github.io/audio/salamander/',
+    urls: {
+        C1: 'C1.mp3',
+        C2: 'C2.mp3',
+        C3: 'C3.mp3',
+        C4: 'C4.mp3',
+        'D#4': 'Ds4.mp3',
+        'F#4': 'Fs4.mp3',
+        A4: 'A4.mp3',
+    },
+    volume: -8,
+    release: 1,
+    baseUrl: 'https://tonejs.github.io/audio/salamander/',
 }).toDestination();
 
 const drumCompress = new Tone.Compressor({
-  threshold: -30,
-  ratio: 10,
-  attack: 0.01,
-  release: 0.2,
+    threshold: -30,
+    ratio: 10,
+    attack: 0.01,
+    release: 0.2,
 }).toDestination();
 
 const distortion = new Tone.Distortion({
-  distortion: 0.4,
-  wet: 0.4,
+    distortion: 0.4,
+    wet: 0.4,
 });
 
 const snare = new Tone.Player({
-  url: 'https://tonejs.github.io/audio/drum-samples/CR78/snare.mp3',
-  volume: -25,
-  fadeOut: 0.1,
+    url: 'https://tonejs.github.io/audio/drum-samples/CR78/snare.mp3',
+    volume: -25,
+    fadeOut: 0.1,
 }).chain(distortion, drumCompress);
 
 const hats = new Tone.Player({
-  url: 'https://tonejs.github.io/audio/drum-samples/CR78/hihat.mp3',
-  volume: -10,
-  fadeOut: 0.01,
+    url: 'https://tonejs.github.io/audio/drum-samples/CR78/hihat.mp3',
+    volume: -10,
+    fadeOut: 0.01,
 }).chain(distortion, drumCompress);
 
 const kick = new Tone.MembraneSynth({
-  pitchDecay: 0.02,
-  octaves: 6,
-  oscillator: {
-    type: 'square4',
-  },
-  envelope: {
-    attack: 0.001,
-    decay: 0.2,
-    sustain: 0,
-  },
-  volume: -10,
+    pitchDecay: 0.02,
+    octaves: 6,
+    oscillator: {
+        type: 'square4',
+    },
+    envelope: {
+        attack: 0.001,
+        decay: 0.2,
+        sustain: 0,
+    },
+    volume: -10,
 }).connect(drumCompress);
 
 const bass = new Tone.FMSynth({
-  harmonicity: 1,
-  modulationIndex: 3.5,
-  oscillator: {
-    type: 'custom',
-    partials: [0, 1, 0, 2],
-  },
-  envelope: {
-    attack: 0.08,
-    decay: 0.3,
-    sustain: 0,
-  },
-  modulation: {
-    type: 'square',
-  },
-  modulationEnvelope: {
-    attack: 0.1,
-    decay: 0.2,
-    sustain: 0.3,
-    release: 0.01,
-  },
+    harmonicity: 1,
+    modulationIndex: 3.5,
+    oscillator: {
+        type: 'custom',
+        partials: [0, 1, 0, 2],
+    },
+    envelope: {
+        attack: 0.08,
+        decay: 0.3,
+        sustain: 0,
+    },
+    modulation: {
+        type: 'square',
+    },
+    modulationEnvelope: {
+        attack: 0.1,
+        decay: 0.2,
+        sustain: 0.3,
+        release: 0.01,
+    },
 }).toDestination();
 const monoBass = new Tone.MonoSynth({
-  volume: -16,
-  envelope: {
-    attack: 0.01,
-    decay: 0.01,
-    release: 0.01,
-  },
-  filterEnvelope: {
-    attack: 0.001,
-    decay: 0.01,
-    sustain: 0.1,
-    baseFrequency: 200,
-    octaves: 1.6,
-  },
+    volume: -16,
+    envelope: {
+        attack: 0.01,
+        decay: 0.01,
+        release: 0.01,
+    },
+    filterEnvelope: {
+        attack: 0.001,
+        decay: 0.01,
+        sustain: 0.1,
+        baseFrequency: 200,
+        octaves: 1.6,
+    },
 }).toDestination();
 
 /*
@@ -122,259 +122,263 @@ So the kick group has parameters set by 8 hex characters
 */
 
 const segmentDrumChars = (char) => {
-  switch (char) {
-    case '0':
-    case '1':
-    case '2':
-    case '3':
-      return ['-', '-'];
-    case '4':
-    case '5':
-    case '6':
-    case '7':
-      return ['*', '-'];
-    case '8':
-    case '9':
-    case 'a':
-    case 'b':
-      return ['-', '*'];
-    case 'c':
-    case 'd':
-    case 'e':
-    case 'f':
-      return ['*', '*'];
-    default:
-      return -1;
-  }
+    switch (char) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+            return ['-', '-'];
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+            return ['*', '-'];
+        case '8':
+        case '9':
+        case 'a':
+        case 'b':
+            return ['-', '*'];
+        case 'c':
+        case 'd':
+        case 'e':
+        case 'f':
+            return ['*', '*'];
+        default:
+            return -1;
+    }
 };
 
 const segmentKeyChars = (char) => {
-  switch (char) {
-    case '0':
-    case '1':
-      return ['', ''];
-    case '2':
-      return ['C3', ''];
-    case '3':
-      return ['D3', ''];
-    case '4':
-      return ['E3', ''];
-    case '5':
-      return ['F3', ''];
-    case '6':
-      return ['G3', ''];
-    case '7':
-      return ['A3', ''];
-    case '8':
-      return ['', 'C3'];
-    case '9':
-      return ['', 'D3'];
-    case 'a':
-      return ['', 'E3'];
-    case 'b':
-      return ['', 'F3'];
-    case 'c':
-      return ['G3', 'C3'];
-    case 'd':
-      return ['A3', 'D3'];
-    case 'e':
-      return ['B3', 'E3'];
-    case 'f':
-      return ['C4', 'F3'];
-    default:
-      return -1;
-  }
+    switch (char) {
+        case '0':
+        case '1':
+            return ['', ''];
+        case '2':
+            return ['C3', ''];
+        case '3':
+            return ['D3', ''];
+        case '4':
+            return ['E3', ''];
+        case '5':
+            return ['F3', ''];
+        case '6':
+            return ['G3', ''];
+        case '7':
+            return ['A3', ''];
+        case '8':
+            return ['', 'C3'];
+        case '9':
+            return ['', 'D3'];
+        case 'a':
+            return ['', 'E3'];
+        case 'b':
+            return ['', 'F3'];
+        case 'c':
+            return ['G3', 'C3'];
+        case 'd':
+            return ['A3', 'D3'];
+        case 'e':
+            return ['B3', 'E3'];
+        case 'f':
+            return ['C4', 'F3'];
+        default:
+            return -1;
+    }
 };
 
 const segmentBassChars = (char) => {
-  switch (char) {
-    case '0':
-    case '1':
-      return ['', ''];
-    case '2':
-      return ['C1', ''];
-    case '3':
-      return ['D1', ''];
-    case '4':
-      return ['E1', ''];
-    case '5':
-      return ['F1', ''];
-    case '6':
-      return ['G1', ''];
-    case '7':
-      return ['A1', ''];
-    case '8':
-      return ['', 'C1'];
-    case '9':
-      return ['', 'D1'];
-    case 'a':
-      return ['', 'E1'];
-    case 'b':
-      return ['', 'F1'];
-    case 'c':
-      return ['G1', 'C1'];
-    case 'd':
-      return ['A1', 'D1'];
-    case 'e':
-      return ['B1', 'E1'];
-    case 'f':
-      return ['C1', 'F1'];
-    default:
-      return -1;
-  }
+    switch (char) {
+        case '0':
+        case '1':
+            return ['', ''];
+        case '2':
+            return ['C1', ''];
+        case '3':
+            return ['D1', ''];
+        case '4':
+            return ['E1', ''];
+        case '5':
+            return ['F1', ''];
+        case '6':
+            return ['G1', ''];
+        case '7':
+            return ['A1', ''];
+        case '8':
+            return ['', 'C1'];
+        case '9':
+            return ['', 'D1'];
+        case 'a':
+            return ['', 'E1'];
+        case 'b':
+            return ['', 'F1'];
+        case 'c':
+            return ['G1', 'C1'];
+        case 'd':
+            return ['A1', 'D1'];
+        case 'e':
+            return ['B1', 'E1'];
+        case 'f':
+            return ['C1', 'F1'];
+        default:
+            return -1;
+    }
 };
 
 const charsToDrumInput = (charStr) => {
-  const output = [[], [], [], []];
-  let counter = 0;
-  for (const idx in charStr) {
-    const char = charStr[idx];
-    const pair = segmentDrumChars(char);
-    output[counter].push(...pair);
-    if ((idx % 2) === 1) {
-      counter++;
+    const output = [[], [], [], []];
+    let counter = 0;
+    for (const idx in charStr) {
+        const char = charStr[idx];
+        const pair = segmentDrumChars(char);
+        output[counter].push(...pair);
+        if ((idx % 2) === 1) {
+            counter++;
+        }
     }
-  }
 
-  return output;
+    return output;
 };
 
 const charsToKeysInput = (charStr) => {
-  const output = [[], [], [], []];
-  let counter = 0;
-  for (const idx in charStr) {
-    const char = charStr[idx];
-    const pair = segmentKeyChars(char);
-    output[counter].push(...pair);
-    if ((idx % 2) === 1) {
-      counter++;
+    const output = [[], [], [], []];
+    let counter = 0;
+    for (const idx in charStr) {
+        const char = charStr[idx];
+        const pair = segmentKeyChars(char);
+        output[counter].push(...pair);
+        if ((idx % 2) === 1) {
+            counter++;
+        }
     }
-  }
 
-  return output;
+    return output;
 };
 
 const charsToBassInput = (charStr) => {
-  const output = [[], [], [], []];
-  let counter = 0;
-  for (const idx in charStr) {
-    const char = charStr[idx];
-    const pair = segmentBassChars(char);
-    output[counter].push(...pair);
-    if ((idx % 2) === 1) {
-      counter++;
+    const output = [[], [], [], []];
+    let counter = 0;
+    for (const idx in charStr) {
+        const char = charStr[idx];
+        const pair = segmentBassChars(char);
+        output[counter].push(...pair);
+        if ((idx % 2) === 1) {
+            counter++;
+        }
     }
-  }
 
-  return output;
+    return output;
 };
 
+let lastAddress;
 export const generateAudioFromWallet = (address = WALLET_ADDRESS) => {
-  const splitArr = address.split('x');
-  const fortyChars = splitArr[1].toLowerCase();
+    lastAddress = address
 
-  console.log('fortyChars', fortyChars, fortyChars.length);
+    const splitArr = address.split('x');
+    const fortyChars = splitArr[1].toLowerCase();
 
-  const kickInput = charsToDrumInput(fortyChars.substring(0, 8));
-  console.log('kickInput', kickInput);
-  const kickSeq = new Tone.Sequence((time, note) => {
-    if (note === '*') {
-      kick.triggerAttack('C1', time);
-    }
-    // synth.triggerAttackRelease(note, 0.1, time);
-    // subdivisions are given as subarrays
-  }, kickInput).start(0);
+    console.log('fortyChars', fortyChars, fortyChars.length);
 
-  const snareInput = charsToDrumInput(fortyChars.substring(8, 16));
-  console.log('snareInput', snareInput);
-  const snareSeq = new Tone.Sequence((time, note) => {
-    if (note === '*') {
-      snare.start(time);
-    }
-  }, snareInput).start(0);
+    const kickInput = charsToDrumInput(fortyChars.substring(0, 8));
+    console.log('kickInput', kickInput);
+    const kickSeq = new Tone.Sequence((time, note) => {
+        if (note === '*') {
+            kick.triggerAttack('C1', time);
+        }
+        // synth.triggerAttackRelease(note, 0.1, time);
+        // subdivisions are given as subarrays
+    }, kickInput).start(0);
 
-  const hihatInput = charsToDrumInput(fortyChars.substring(16, 24));
-  console.log('hihatInput', hihatInput);
-  const hihatSeq = new Tone.Sequence((time, note) => {
-    if (note === '*') {
-      hats.start(time);
-    }
-  }, hihatInput).start(0);
+    const snareInput = charsToDrumInput(fortyChars.substring(8, 16));
+    console.log('snareInput', snareInput);
+    const snareSeq = new Tone.Sequence((time, note) => {
+        if (note === '*') {
+            snare.start(time);
+        }
+    }, snareInput).start(0);
 
-  const keysInput = charsToKeysInput(fortyChars.substring(24, 32));
-  console.log('keysInput', keysInput);
-  const keysSeq = new Tone.Sequence((time, note) => {
-    if (note !== '') {
-      console.log('time', time, note);
-      salamanderKeys.triggerAttackRelease(note, '8n');
-    }
-  }, keysInput).start(0);
+    const hihatInput = charsToDrumInput(fortyChars.substring(16, 24));
+    console.log('hihatInput', hihatInput);
+    const hihatSeq = new Tone.Sequence((time, note) => {
+        if (note === '*') {
+            hats.start(time);
+        }
+    }, hihatInput).start(0);
 
-  const bassInput = charsToBassInput(fortyChars.substring(32, 40));
-  console.log('bassInput', bassInput);
-  const bassSeq = new Tone.Sequence((time, note) => {
-    if (note !== '') {
-      console.log('time', time, note);
-      monoBass.triggerAttackRelease(note, '16n');
-    }
-  }, bassInput).start(0);
+    const keysInput = charsToKeysInput(fortyChars.substring(24, 32));
+    console.log('keysInput', keysInput);
+    const keysSeq = new Tone.Sequence((time, note) => {
+        if (note !== '') {
+            console.log('time', time, note);
+            salamanderKeys.triggerAttackRelease(note, '8n');
+        }
+    }, keysInput).start(0);
 
-  /*
-    const loop = new Tone.Loop((time) => {
-        // triggered every eighth note.
-        console.log(time)
-        casioKeys.player(0).start(time, 0, "16n");
-    }, "16n").start(0)
-    */
+    const bassInput = charsToBassInput(fortyChars.substring(32, 40));
+    console.log('bassInput', bassInput);
+    const bassSeq = new Tone.Sequence((time, note) => {
+        if (note !== '') {
+            console.log('time', time, note);
+            monoBass.triggerAttackRelease(note, '16n');
+        }
+    }, bassInput).start(0);
 
-  Tone.Transport.bpm.value = DEFAULT_BPM;
+    /*
+      const loop = new Tone.Loop((time) => {
+          // triggered every eighth note.
+          console.log(time)
+          casioKeys.player(0).start(time, 0, "16n");
+      }, "16n").start(0)
+      */
+
+    Tone.Transport.bpm.value = DEFAULT_BPM;
+    //Tone.Transport.position = '0:0:0';
 };
 
 let isInitialized = false;
-export const togglePlayback = (nextPlayState) => {
-  console.log('togglePlayback', nextPlayState);
-  if (!isInitialized) {
-    Tone.start();
-    generateAudioFromWallet();
-    isInitialized = true;
-    console.log('context started');
-  }
+export const togglePlayback = async (nextPlayState) => {
+    console.log('togglePlayback', nextPlayState);
+    if (!isInitialized) {
+        await Tone.start();
+        generateAudioFromWallet();
+        isInitialized = true;
+        console.log('context started');
+    }
 
-  if (!nextPlayState) {
-    console.log('STOP');
-    Tone.Transport.stop();
-  } else {
-    console.log('START');
-    Tone.Transport.position = 0;
-    Tone.Transport.start();
-  }
+    if (!nextPlayState) {
+        console.log('STOP');
+        Tone.Transport.stop();
+    } else {
+        console.log('START');
+        Tone.Transport.start();
+    }
 };
 
-export const downloadAudio = () => {
-  console.log('downloadAudio', isInitialized);
-  Tone.start();
+export const downloadAudio = async () => {
+    console.log('downloadAudio', isInitialized);
+    if (!isInitialized) {
+        await Tone.start();
+        generateAudioFromWallet();
+    }
 
-  generateAudioFromWallet();
-
-  const recorder = new Tone.Recorder();
-  /// /Tone.getDestination().connect(recorder);
-  // salamanderKeys.disconnect(Tone.getDestination())
-  // salamanderKeys.connect(recorder)
-  Tone.getDestination().connect(recorder);
-  recorder.start();
-  Tone.Transport.position = 0;
-  Tone.Transport.start();
-
-  setTimeout(async () => {
-    console.log('DOWNLOAD');
+    const recorder = new Tone.Recorder();
+    /// /Tone.getDestination().connect(recorder);
+    // salamanderKeys.disconnect(Tone.getDestination())
+    // salamanderKeys.connect(recorder)
+    Tone.getDestination().connect(recorder);
+    recorder.start();
     Tone.Transport.stop();
-    // the recorded audio is returned as a blob
-    const recording = await recorder.stop();
-    // download the recording by creating an anchor element and blob url
-    const url = URL.createObjectURL(recording);
-    const anchor = document.createElement('a');
-    anchor.download = 'myaudio.webm';
-    anchor.href = url;
-    anchor.click();
-  }, 3000);
+    Tone.Transport.start();
+
+    setTimeout(async () => {
+        console.log('DOWNLOAD');
+        Tone.Transport.stop();
+        // the recorded audio is returned as a blob
+        const recording = await recorder.stop();
+        // download the recording by creating an anchor element and blob url
+        const url = URL.createObjectURL(recording);
+        const anchor = document.createElement('a');
+        anchor.download = `artist_audio_${lastAddress}.webm`;
+        anchor.href = url;
+        anchor.click();
+    }, 3000);
 };
