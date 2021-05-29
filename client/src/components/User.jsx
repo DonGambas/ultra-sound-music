b3bfb78fee1d50f684402494d06cfe48dcb5ad7f
Vijay Rudraraju <vijay326@gmail.com>
2021-05-28 20:25:28 -0700

    Download sound as webm

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -1,6 +1,6 @@
-import React  from 'react';
+import React from 'react';
 import Button from 'react-bootstrap/Button';
-import { togglePlayback } from '../audio'
+import { togglePlayback, downloadAudio } from '../audio'
 
 export class User extends React.Component {
   constructor(props) {
@@ -23,13 +23,18 @@ export class User extends React.Component {
           <Button style={{ width: "250px", height: "50px" }} onClick={() => connectMetaMask(setUser)}>Connect Metamask</Button>
         }
         {user.wallet?.length > 0 &&
-          <Button if style={{ width: "250px", height: "50px" }} onClick={() => {
-            togglePlayback(this.state.nextPlayState)
-            this.setState({ nextPlayState: !this.state.nextPlayState })
-          }}>{this.state.nextPlayState ? 'Play Audio' : 'Stop Audio'}</Button>
+          <>
+            <Button if style={{ width: "250px", height: "40px", margin: "8px" }} onClick={() => {
+              togglePlayback(this.state.nextPlayState)
+              this.setState({ nextPlayState: !this.state.nextPlayState })
+            }}>{this.state.nextPlayState ? 'Play Audio' : 'Stop Audio'}</Button>
+            <Button if style={{ width: "250px", height: "40px", margin: "8px" }} onClick={() => {
+              downloadAudio()
+            }}>Download Audio</Button>
+          </>
         }
       </div>
-    );    
+    );
   }
 }
 
